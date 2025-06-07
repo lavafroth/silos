b3fc39afd9d791d5c27c37480c9dc48f875924b1
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2025-06-07 18:14:57 +0530

    feat: scaffold http server

---
+++ src/embed.rs
@@ -0,0 +1,97 @@
+use super::Args;
+use anyhow::{Error as E, Result};
+use candle_core::Device;
+use candle_core::Tensor;
+use candle_nn::VarBuilder;
+use candle_transformers::models::distilbert::Config;
+use candle_transformers::models::distilbert::DTYPE;
+use candle_transformers::models::distilbert::DistilBertModel;
+use hf_hub::Repo;
+use hf_hub::RepoType;
+use hf_hub::api::sync::Api;
+use std::path::PathBuf;
+use tokenizers::Tokenizer;
+pub struct Embed {
+    model: DistilBertModel,
+    tokenizer: Tokenizer,
+}
+
+impl Embed {
+    pub(crate) fn new(args: Args) -> Result<Self> {
+        let device = if args.cpu {
+            candle_core::Device::Cpu
+        } else {
+            candle_core::Device::new_cuda(0)?
+        };
+
+        let (model_id, revision) = args.resolve_model_and_revision();
+        let (config_path, tokenizer_path, weights_path) =
+            Self::download_model_files(&model_id, &revision)?;
+
+        let config = std::fs::read_to_string(config_path)?;
+        let config: Config = serde_json::from_str(&config)?;
+        let tokenizer = Tokenizer::from_file(tokenizer_path).map_err(E::msg)?;
+
+        let vb = unsafe { VarBuilder::from_mmaped_safetensors(&[weights_path], DTYPE, &device)? };
+        let model = DistilBertModel::load(vb, &config)?;
+
+        Ok(Embed { model, tokenizer })
+    }
+
+    fn download_model_files(model_id: &str, revision: &str) -> Result<(PathBuf, PathBuf, PathBuf)> {
+        let repo = Repo::with_revision(model_id.to_string(), RepoType::Model, revision.to_string());
+        let api = Api::new()?;
+        let api = api.repo(repo);
+
+        let config = api.get("config.json")?;
+        let tokenizer = api.get("tokenizer.json")?;
+        let weights = api.get("model.safetensors")?;
+
+        Ok((config, tokenizer, weights))
+    }
+
+    pub(crate) fn embed(&self, prompt: &str) -> Result<Vec<f32>> {
+        let (token_ids, mask) = prepare_inputs(prompt, &self.tokenizer, &self.model.device)?;
+        let embeddings = self.model.forward(&token_ids, &mask)?;
+        let (_n_sentence, n_tokens, _hidden_size) = embeddings.dims3()?;
+        let embeddings = (embeddings.sum(1)? / (n_tokens as f64))?;
+        let embeddings = normalize_l2(&embeddings)?.reshape(768)?.to_vec1::<f32>()?;
+        Ok(embeddings)
+    }
+}
+
+fn prepare_inputs(
+    prompt: &str,
+    tokenizer: &Tokenizer,
+    device: &Device,
+) -> Result<(Tensor, Tensor)> {
+    let mut binding = tokenizer.clone();
+    let tokenizer_configured = binding
+        .with_padding(None)
+        .with_truncation(None)
+        .map_err(E::msg)?;
+
+    let tokens = tokenizer_configured
+        .encode(prompt, true)
+        .map_err(E::msg)?
+        .get_ids()
+        .to_vec();
+
+    let token_ids = Tensor::new(tokens.as_slice(), device)?.unsqueeze(0)?;
+
+    let mask = attention_mask(tokens.len(), device)?;
+    // println!("token_ids: {:?}", token_ids.to_vec2::<u32>()?);
+
+    Ok((token_ids, mask))
+}
+
+fn attention_mask(size: usize, device: &Device) -> Result<Tensor> {
+    let mask: Vec<_> = (0..size)
+        .flat_map(|i| (0..size).map(move |j| u8::from(j > i)))
+        .collect();
+    Ok(Tensor::from_slice(&mask, (size, size), device)?)
+}
+
+pub fn normalize_l2(v: &Tensor) -> Result<Tensor> {
+    Ok(v.broadcast_div(&v.sqr()?.sum_keepdim(1)?.sqrt()?)?)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,10 @@
-use candle_core::{Device, Tensor};
-use hora::core::ann_index::ANNIndex;
-
-use candle_transformers::models::distilbert::{Config, DTYPE, DistilBertModel};
-use hora::index::hnsw_idx::HNSWIndex;
-
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, post, web};
 use anyhow::{Error as E, Result};
-use candle_nn::VarBuilder;
 use clap::Parser;
-use hf_hub::{Repo, RepoType, api::sync::Api};
-use std::path::PathBuf;
-use tokenizers::Tokenizer;
+use hora::core::ann_index::ANNIndex;
+use hora::index::hnsw_idx::HNSWIndex;
+use serde::{Deserialize, Serialize};
+mod embed;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -32,27 +27,6 @@ struct Args {
 }
 
 impl Args {
-    fn build_embed(&self) -> Result<Embed> {
-        let device = if self.cpu {
-            candle_core::Device::Cpu
-        } else {
-            candle_core::Device::new_cuda(0)?
-        };
-
-        let (model_id, revision) = self.resolve_model_and_revision();
-        let (config_path, tokenizer_path, weights_path) =
-            self.download_model_files(&model_id, &revision)?;
-
-        let config = std::fs::read_to_string(config_path)?;
-        let config: Config = serde_json::from_str(&config)?;
-        let tokenizer = Tokenizer::from_file(tokenizer_path).map_err(E::msg)?;
-
-        let vb = self.load_variables(&weights_path, &device)?;
-        let model = DistilBertModel::load(vb, &config)?;
-
-        Ok(Embed { model, tokenizer })
-    }
-
     fn resolve_model_and_revision(&self) -> (String, String) {
         let default_model = "distilbert-base-uncased".to_string();
         let default_revision = "main".to_string();
@@ -64,39 +38,43 @@ impl Args {
             (None, None) => (default_model, default_revision),
         }
     }
-
-    fn download_model_files(
-        &self,
-        model_id: &str,
-        revision: &str,
-    ) -> Result<(PathBuf, PathBuf, PathBuf)> {
-        let repo = Repo::with_revision(model_id.to_string(), RepoType::Model, revision.to_string());
-        let api = Api::new()?;
-        let api = api.repo(repo);
-
-        let config = api.get("config.json")?;
-        let tokenizer = api.get("tokenizer.json")?;
-        let weights = api.get("model.safetensors")?;
-
-        Ok((config, tokenizer, weights))
-    }
-
-    fn load_variables(&self, weights_path: &PathBuf, device: &Device) -> Result<VarBuilder> {
-        Ok(unsafe { VarBuilder::from_mmaped_safetensors(&[weights_path], DTYPE, device)? })
-    }
 }
 
-fn main() -> Result<()> {
+#[derive(Serialize)]
+pub struct SnippetResponse {
+    id: usize,
+    snippet: Snippet,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Snippet {
+    lang: String,
+    desc: String,
+    body: String,
+}
+
+#[get("/api/v1/get")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("bob")
+}
+
+#[post("/api/v1/add")]
+async fn add_snippet(snippet: web::Json<Snippet>) -> impl Responder {
+    HttpResponse::Ok().body(format!(
+        "{} {} {}",
+        snippet.body, snippet.lang, snippet.desc
+    ))
+}
+
+#[actix_web::main]
+async fn main() -> Result<()> {
     let args = Args::parse();
     let prompt = args.prompt.clone();
-    let embed = args.build_embed()?;
+    let embed = embed::Embed::new(args)?;
 
     let dimension = 768;
-
-    let mut index = HNSWIndex::<f32, usize>::new(
-        dimension,
-        &hora::index::hnsw_params::HNSWParams::<f32>::default(),
-    );
+    let params = hora::index::hnsw_params::HNSWParams::<f32>::default();
+    let mut index = HNSWIndex::<f32, usize>::new(dimension, &params);
 
     let strings = ["lol"];
 
@@ -119,56 +97,9 @@ fn main() -> Result<()> {
         .collect();
     println!("target has neighbors: {:?}", nn);
 
-    Ok(())
-}
-
-pub struct Embed {
-    model: DistilBertModel,
-    tokenizer: Tokenizer,
-}
-
-impl Embed {
-    fn embed(&self, prompt: &str) -> Result<Vec<f32>> {
-        let (token_ids, mask) = prepare_inputs(prompt, &self.tokenizer, &self.model.device)?;
-        let embeddings = self.model.forward(&token_ids, &mask)?;
-        let (_n_sentence, n_tokens, _hidden_size) = embeddings.dims3()?;
-        let embeddings = (embeddings.sum(1)? / (n_tokens as f64))?;
-        let embeddings = normalize_l2(&embeddings)?.reshape(768)?.to_vec1::<f32>()?;
-        Ok(embeddings)
-    }
-}
-fn prepare_inputs(
-    prompt: &str,
-    tokenizer: &Tokenizer,
-    device: &Device,
-) -> Result<(Tensor, Tensor)> {
-    let mut binding = tokenizer.clone();
-    let tokenizer_configured = binding
-        .with_padding(None)
-        .with_truncation(None)
-        .map_err(E::msg)?;
-
-    let tokens = tokenizer_configured
-        .encode(prompt, true)
-        .map_err(E::msg)?
-        .get_ids()
-        .to_vec();
-
-    let token_ids = Tensor::new(tokens.as_slice(), device)?.unsqueeze(0)?;
-
-    let mask = attention_mask(tokens.len(), device)?;
-    println!("token_ids: {:?}", token_ids.to_vec2::<u32>()?);
-
-    Ok((token_ids, mask))
-}
-
-fn attention_mask(size: usize, device: &Device) -> Result<Tensor> {
-    let mask: Vec<_> = (0..size)
-        .flat_map(|i| (0..size).map(move |j| u8::from(j > i)))
-        .collect();
-    Ok(Tensor::from_slice(&mask, (size, size), device)?)
-}
-
-pub fn normalize_l2(v: &Tensor) -> Result<Tensor> {
-    Ok(v.broadcast_div(&v.sqr()?.sum_keepdim(1)?.sqrt()?)?)
+    HttpServer::new(|| App::new().service(hello).service(add_snippet))
+        .bind(("127.0.0.1", 8000))?
+        .run()
+        .await
+        .map_err(E::from)
 }
