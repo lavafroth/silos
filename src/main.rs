5f3984ee488656b743aeff9d958d652b9d38c0fe
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2025-06-07 14:10:30 +0530

    feat: move embedding into its own state

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,15 @@
-use hora::core::ann_index::ANNIndex;
 use candle_core::{Device, Tensor};
+use hora::core::ann_index::ANNIndex;
 
-use candle_transformers::models::distilbert::{
-    Config, DTYPE, DistilBertModel,
-};
+use candle_transformers::models::distilbert::{Config, DTYPE, DistilBertModel};
+use hora::index::hnsw_idx::HNSWIndex;
 
 use anyhow::{Error as E, Result};
 use candle_nn::VarBuilder;
-use clap::{Parser, ValueEnum};
+use clap::Parser;
 use hf_hub::{Repo, RepoType, api::sync::Api};
 use std::path::PathBuf;
 use tokenizers::Tokenizer;
-
-
-#[derive(Clone, Debug, Copy, PartialEq, Eq, ValueEnum)]
-enum Which {
-    #[value(name = "distilbert")]
-    DistilBert,
-}
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -40,8 +32,12 @@ struct Args {
 }
 
 impl Args {
-    fn build_model_and_tokenizer(&self) -> Result<(DistilBertModel, Tokenizer)> {
-        let device = candle_core::Device::Cpu;
+    fn build_embed(&self) -> Result<Embed> {
+        let device = if self.cpu {
+            candle_core::Device::Cpu
+        } else {
+            candle_core::Device::new_cuda(0)?
+        };
 
         let (model_id, revision) = self.resolve_model_and_revision();
         let (config_path, tokenizer_path, weights_path) =
@@ -54,7 +50,7 @@ impl Args {
         let vb = self.load_variables(&weights_path, &device)?;
         let model = DistilBertModel::load(vb, &config)?;
 
-        Ok((model, tokenizer))
+        Ok(Embed { model, tokenizer })
     }
 
     fn resolve_model_and_revision(&self) -> (String, String) {
@@ -80,43 +76,32 @@ impl Args {
 
         let config = api.get("config.json")?;
         let tokenizer = api.get("tokenizer.json")?;
-            let weights = api.get("model.safetensors")?;
+        let weights = api.get("model.safetensors")?;
 
         Ok((config, tokenizer, weights))
     }
 
     fn load_variables(&self, weights_path: &PathBuf, device: &Device) -> Result<VarBuilder> {
-            Ok(unsafe { VarBuilder::from_mmaped_safetensors(&[weights_path], DTYPE, device)? })
+        Ok(unsafe { VarBuilder::from_mmaped_safetensors(&[weights_path], DTYPE, device)? })
     }
 }
 
 fn main() -> Result<()> {
     let args = Args::parse();
-
-    let (model, tokenizer) = args.build_model_and_tokenizer()?;
-    let device = &model.device;
-
-    let (token_ids, mask) = prepare_inputs(&args, &tokenizer, device)?;
-    let embeddings = model.forward(&token_ids, &mask)?;
-    // Apply some avg-pooling by taking the mean embedding value for all tokens (including padding)
-    let (_n_sentence, n_tokens, _hidden_size) = embeddings.dims3()?;
-    let embeddings = (embeddings.sum(1)? / (n_tokens as f64))?;
-    let embeddings = normalize_l2(&embeddings)?.reshape(768)?.to_vec1::<f32>();
+    let prompt = args.prompt.clone();
+    let embed = args.build_embed()?;
+    let embeddings = embed.embed(&prompt)?;
     println!("{embeddings:?}");
 
-        let dimension = 768;
+    let dimension = 768;
 
-        // init index
-        let mut index = hora::index::hnsw_idx::HNSWIndex::<f32, usize>::new(
-            dimension,
-            &hora::index::hnsw_params::HNSWParams::<f32>::default(),
-        );
+    let mut index = HNSWIndex::<f32, usize>::new(
+        dimension,
+        &hora::index::hnsw_params::HNSWParams::<f32>::default(),
+    );
 
-        // add point
-        // for (i, sample) in samples.iter().enumerate().take(n) {
-        //     index.add(sample, i).unwrap();
-        // }
-        index.build(hora::core::metrics::Metric::Euclidean).unwrap();
+    // index.add(sample, i).unwrap();
+    index.build(hora::core::metrics::Metric::Euclidean).unwrap();
 
     //     let mut rng = thread_rng();
     //     let target: usize = rng.gen_range(0..n);
@@ -129,7 +114,27 @@ fn main() -> Result<()> {
 
     Ok(())
 }
-fn prepare_inputs(args: &Args, tokenizer: &Tokenizer, device: &Device) -> Result<(Tensor, Tensor)> {
+
+pub struct Embed {
+    model: DistilBertModel,
+    tokenizer: Tokenizer,
+}
+
+impl Embed {
+    fn embed(&self, prompt: &str) -> Result<Vec<f32>> {
+        let (token_ids, mask) = prepare_inputs(prompt, &self.tokenizer, &self.model.device)?;
+        let embeddings = self.model.forward(&token_ids, &mask)?;
+        let (_n_sentence, n_tokens, _hidden_size) = embeddings.dims3()?;
+        let embeddings = (embeddings.sum(1)? / (n_tokens as f64))?;
+        let embeddings = normalize_l2(&embeddings)?.reshape(768)?.to_vec1::<f32>()?;
+        Ok(embeddings)
+    }
+}
+fn prepare_inputs(
+    prompt: &str,
+    tokenizer: &Tokenizer,
+    device: &Device,
+) -> Result<(Tensor, Tensor)> {
     let mut binding = tokenizer.clone();
     let tokenizer_configured = binding
         .with_padding(None)
@@ -137,12 +142,12 @@ fn prepare_inputs(args: &Args, tokenizer: &Tokenizer, device: &Device) -> Result
         .map_err(E::msg)?;
 
     let tokens = tokenizer_configured
-        .encode(args.prompt.clone(), true)
+        .encode(prompt, true)
         .map_err(E::msg)?
         .get_ids()
         .to_vec();
 
-    let token_ids = Tensor::new(&tokens[..], device)?.unsqueeze(0)?;
+    let token_ids = Tensor::new(tokens.as_slice(), device)?.unsqueeze(0)?;
 
     let mask = attention_mask(tokens.len(), device)?;
     println!("token_ids: {:?}", token_ids.to_vec2::<u32>()?);
@@ -150,14 +155,12 @@ fn prepare_inputs(args: &Args, tokenizer: &Tokenizer, device: &Device) -> Result
     Ok((token_ids, mask))
 }
 
-
 fn attention_mask(size: usize, device: &Device) -> Result<Tensor> {
     let mask: Vec<_> = (0..size)
         .flat_map(|i| (0..size).map(move |j| u8::from(j > i)))
         .collect();
     Ok(Tensor::from_slice(&mask, (size, size), device)?)
 }
-
 
 pub fn normalize_l2(v: &Tensor) -> Result<Tensor> {
     Ok(v.broadcast_div(&v.sqr()?.sum_keepdim(1)?.sqrt()?)?)
